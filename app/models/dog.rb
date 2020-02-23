f2f3e10e393fbbdd07718f0eddc05ce259d4514b
ryan m <contact@ryanmeek.com>
2020-02-23 16:43:18 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,12 +3,9 @@ class Dog
   @@all = []
 
   def initialize(name, breed, age)
-    self.name = name
-    self.breed = breed
-    self.age = age
-    @@all << self
+    @name = name; @breed = breed; @age = age; @@all << self
   end
 
-  def self.all() @@all end
+  def self.all() @@all
 
 end
